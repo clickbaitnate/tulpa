2bb2e1c51d449d78eab84e282fd5f9ead48c0f1e
Nathan Marotz <nathan.marotz@gmail.com>
2021-01-29 20:12:36 -0600

    Reimagining Divination and Currency

---
+++ main.js
@@ -0,0 +1,72 @@
+const SHA256 = require('crypto-js/sha256')
+
+class Block{
+    constructor(index, timestamp, data, previousHash = ''){
+      this.index = index;
+      this.timestamp = timestamp;
+      this.data = data;
+      this.previousHash = previousHash;
+      this.hash = this.calculateHash();  
+      this.nonce = 0;
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !==Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined: " + this.hash);
+    }
+}
+
+class Blockchain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 5;
+    }
+
+    createGenesisBlock(){
+        return new Block(0, "01/28/2021", { amount: 3});
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+let tulpa = new Blockchain();
+console.log('Mining block 1...');
+tulpa.addBlock(new Block(1, "01/28/2021", { amount: 4}));
+console.log('Mining block 2...');
+tulpa.addBlock(new Block(2, "01/28/2021", { amount: 14}));
+
+
+//console.log(JSON.stringify(tulpa, null, 4));
